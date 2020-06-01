756771750f216dd40b04da6d01d8995d563b9b99
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-06-01 23:34:17 +0100

    implement button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class Button extends React.Component {
-  render(){}
-}
+const Button = props => {
+  const { name } = props;
+  return (
+    <div id="button">
+      { name }
+    </div>
+  );
+};
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Button;
