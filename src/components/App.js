02df9fc50cdcc16299c2d1a72ec66355f24d456c
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-06-02 00:44:41 +0100

    make all element flex

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div id="app">
       <Display />
       <ButtonPanel />
     </div>
