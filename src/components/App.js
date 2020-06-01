77e7845f87413065a66a3d639b72157da3e62271
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-06-01 23:01:45 +0100

    add components files and folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return <h1 className="App">Math Magiciens is working</h1>;
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default class ButtonPanel extends React.Component {
+  render(){}
+}
