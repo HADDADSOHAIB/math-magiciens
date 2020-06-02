231fc098fe4c2b2b0b2e1b0822efd7eadcffe15c
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-06-02 13:31:06 +0100

    add calculate to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
+/* eslint-disable react/no-unused-state */
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div id="app">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handelUserInput(e) {
+    const current = this.state;
+    this.setState(calculate(current, e.target.value));
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
 
 export default App;
