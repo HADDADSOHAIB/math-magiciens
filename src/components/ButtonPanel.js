4a60240402849a1ee7c1f843ef028df098c86f48
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-06-02 01:18:38 +0100

    add bg color and wide to the button props

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,32 +4,32 @@ import Button from './Button';
 const ButtonPanel = () => (
   <div id="button-panel">
     <div className="group">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
+      <Button name="AC" bgColor="#eee" />
+      <Button name="+/-" bgColor="#eee" />
+      <Button name="%" bgColor="#eee" />
       <Button name="/" />
     </div>
     <div className="group">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
+      <Button name="7" bgColor="#eee" />
+      <Button name="8" bgColor="#eee" />
+      <Button name="9" bgColor="#eee" />
       <Button name="X" />
     </div>
     <div className="group">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
+      <Button name="4" bgColor="#eee" />
+      <Button name="5" bgColor="#eee" />
+      <Button name="6" bgColor="#eee" />
       <Button name="-" />
     </div>
     <div className="group">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
+      <Button name="1" bgColor="#eee" />
+      <Button name="2" bgColor="#eee" />
+      <Button name="3" bgColor="#eee" />
       <Button name="+" />
     </div>
     <div className="group button-zero">
-      <Button name="0" />
-      <Button name="." />
+      <Button name="0" bgColor="#eee" wide />
+      <Button name="." bgColor="#eee" />
       <Button name="=" />
     </div>
   </div>
