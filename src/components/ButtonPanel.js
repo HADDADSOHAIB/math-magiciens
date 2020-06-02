ad0d6deb6f003f9128d2ff76cd4b6bd40f365d21
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-06-02 01:07:17 +0100

    style panel and buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,31 +3,31 @@ import Button from './Button';
 
 const ButtonPanel = () => (
   <div id="button-panel">
-    <div id="group-1">
+    <div className="group">
       <Button name="AC" />
       <Button name="+/-" />
       <Button name="%" />
       <Button name="/" />
     </div>
-    <div id="group-2">
+    <div className="group">
       <Button name="7" />
       <Button name="8" />
       <Button name="9" />
       <Button name="X" />
     </div>
-    <div id="group-3">
+    <div className="group">
       <Button name="4" />
       <Button name="5" />
       <Button name="6" />
       <Button name="-" />
     </div>
-    <div id="group-4">
+    <div className="group">
       <Button name="1" />
       <Button name="2" />
       <Button name="3" />
       <Button name="+" />
     </div>
-    <div id="group-5">
+    <div className="group button-zero">
       <Button name="0" />
       <Button name="." />
       <Button name="=" />
