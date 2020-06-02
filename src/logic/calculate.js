c97b100de7af15cc88b217f32b93f0fdcd406511
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-06-02 12:57:52 +0100

    implement calculate function

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -9,15 +9,7 @@ const calculate = ({ total, next, operation }, buttonName) => {
     };
   }
 
-  if (buttonName.match(/[-/X+]/) && next === '') {
-    return {
-      total,
-      next,
-      operation,
-    };
-  }
-
-  if (buttonName.match(/[-/X+]/) && next !== '') {
+  if (buttonName.match(/[-/X+%]/) && next) {
     return {
       total: next,
       next: '',
@@ -25,6 +17,59 @@ const calculate = ({ total, next, operation }, buttonName) => {
     };
   }
 
-}
+  if (buttonName === '.' && !next.includes('.')) {
+    return {
+      total,
+      next: `${next}.`,
+      operation,
+    };
+  }
+
+  if (buttonName === 'AC') {
+    return {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  if (buttonName === '+/-' && next) {
+    return {
+      total,
+      next: (-1 * parseFloat(next)).toString(),
+      operation,
+    };
+  }
+
+  if (buttonName === '+/-' && !next && total) {
+    return {
+      total: (-1 * parseFloat(total)).toString(),
+      next,
+      operation,
+    };
+  }
+
+  if (buttonName === '=' && next && total) {
+    return {
+      total: (operate(parseFloat(next), parseFloat(total), operation)).toString(),
+      next: null,
+      operation: null,
+    };
+  }
+
+  if (buttonName === '=' && !next && total && operation === '%') {
+    return {
+      total: (operate(parseFloat(total), null, operation)).toString(),
+      next: null,
+      operation: null,
+    };
+  }
+
+  return {
+    total,
+    next,
+    operation,
+  };
+};
 
 export default calculate;
